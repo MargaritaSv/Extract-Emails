9f176ba0d722ba0d9bd7bf37724912e894e718e3
MargaritaSv <margarita.esti@gmail.com>
2015-10-04 12:12:19 +0300

    code

---
+++ Extract1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Extract1
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        string pattern = @"(?<=\s|^)([a-z0-9]+(?:[_.-][a-z0-9]+)*@[a-z]+\-?[a-z]+(?:\.[a-z]+)+)\b";
+
+        Regex rgx = new Regex(pattern);
+        MatchCollection mts = rgx.Matches(input);
+
+        foreach (Match email in mts)
+        {
+            Console.WriteLine(email.Groups[0]);
+        }
+    }
+}
